150bb2a3ccdffda7055091485a8e5611a3e07329
Nick Lagges <nlagges@mail.wlu.edu>
2024-04-15 22:17:58 -0400

    results shown

---
+++ countryStats.py
@@ -0,0 +1,15 @@
+"""
+Nick Lagges
+
+Country
+"""
+
+class CountryStats:
+    
+    def __init__(self, econStren, qol, pol, dip, infra, defense):
+        self.ECONOMIC_STRENGTH = econStren
+        self.QUALITY_OF_LIFE = qol
+        self.POLITICAL_STABILITY = pol
+        self.DIPLOMACY = dip
+        self.INFRASTRUCTURE = infra
+        self.DEFENSE = defense
